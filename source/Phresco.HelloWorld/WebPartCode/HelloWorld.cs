37c4bc9f7f4c9ba54b6bf59864fdfdcd28563503
ganeshkumar-s <ganeshkumar.sivalingam@photoninfotech.net>
2012-08-16 13:09:57 +0530

    added new files

---
+++ source/Phresco.HelloWorld/WebPartCode/HelloWorld.cs
@@ -0,0 +1,116 @@
+/*
+ * ###
+ * Archetype - Sharepoint
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Phresco.HelloWorld
+{
+    [Guid("e91fb3c5-4acb-468b-b588-5371341542a3")]
+    public class HelloWorld : Microsoft.SharePoint.WebPartPages.WebPart
+    {
+        private bool _error = false;
+        private string _myProperty = null;
+
+
+        [Personalizable(PersonalizationScope.Shared)]
+        [WebBrowsable(true)]
+        [System.ComponentModel.Category("My Property Group")]
+        [WebDisplayName("MyProperty")]
+        [WebDescription("Meaningless Property")]
+        public string MyProperty
+        {
+            get
+            {
+                if (_myProperty == null)
+                {
+                    _myProperty = "Hello World";
+                }
+                return _myProperty;
+            }
+            set { _myProperty = value; }
+        }
+
+
+        public HelloWorld()
+        {
+            this.ExportMode = WebPartExportMode.All;
+        }
+
+        /// <summary>
+        /// Create all your controls here for rendering.
+        /// Try to avoid using the RenderWebPart() method.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            if (!_error)
+            {
+                try
+                {
+
+                    base.CreateChildControls();
+
+                    // Your code here...
+                    this.Controls.Add(new LiteralControl(this.MyProperty));
+                }
+                catch (Exception ex)
+                {
+                    HandleException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ensures that the CreateChildControls() is called before events.
+        /// Use CreateChildControls() to create your controls.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            if (!_error)
+            {
+                try
+                {
+                    base.OnLoad(e);
+                    this.EnsureChildControls();
+
+                    // Your code here...
+                }
+                catch (Exception ex)
+                {
+                    HandleException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clear all child controls and add an error message for display.
+        /// </summary>
+        /// <param name="ex"></param>
+        private void HandleException(Exception ex)
+        {
+            this._error = true;
+            this.Controls.Clear();
+            this.Controls.Add(new LiteralControl(ex.Message));
+        }
+    }
+}
